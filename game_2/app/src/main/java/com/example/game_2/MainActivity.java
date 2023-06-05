087b5172698808add8ab949d5c7e286d49893aae
Tematic16 <116188507+Tematic16@users.noreply.github.com>
2023-06-05 13:29:20 +0300

    11

---
+++ game_2/app/src/main/java/com/example/game_2/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.game_2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.game_2.fragment.HomeFragment;
+import com.example.game_2.fragment.UnitsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigstion_view);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id == R.id.user)
+                    replaceFragment(new UnitsFragment());
+                if(id == R.id.home)
+                    replaceFragment(new HomeFragment());
+                if(id == R.id.units)
+                    replaceFragment(new UnitsFragment());
+                return true;
+            }
+        });
+
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+}
